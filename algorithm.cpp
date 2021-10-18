aea2f1b9f5e06630a21749a9fa02149d8dc5af90
novaardiansyah <novaardiansyah78@gmail.com>
2021-10-18 10:00:34 +0700

    basics of "algorithm" and "math.h" functions
    * min() and max()
    * pow()

---
+++ algorithm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+  // ! min() and max() : returns the minimum and maximum values
+  // double x = 22, y = 33;
+  // cout << "minimum value: " << min(x, y) << endl;
+  // cout << "max value: " << max(x, y);
+
+  // ! pow() : returns the power result
+  // double x = 22, y = 33;
+  // cout << "power result " << x << " and " << y << " is: " << pow(x, y);
+
+  cout << endl;
+  return 0;
+}
